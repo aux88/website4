d679034257f5373a2597dd43e87301d4b21a066a
yoshimi.satou <youhes@gmail.com>
2025-08-11 14:44:01 +0900

    ヘッダーメニューの実装を修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,19 +1,19 @@
-var hamburger_button = $('.header__menu-icon');
-var menu = $('.header__menu');
+var hamburger_button = $('.hamburger__menu-icon');
+var menu = $('.hamburger__menu');
 
 // ハンバーガーメニューをクリックしたら
 hamburger_button.on('click', function () {
   // .menuの表示・非表示を繰り返す
-  menu.toggleClass('header__menu-active');
+  menu.toggleClass('hamburger__menu-active');
 });
 
 // 画面幅のサイズが変わったら
 $(window).on('resize', function () {
   // ハンバーガーメニューを閉じる
-  menu.removeClass('header__menu-active');
+  menu.removeClass('hamburger__menu-active');
 });
 
 // 「トップ」のリンクをクリックしたらメニューを閉じる
-$('.header__menu--back-to-top').on('click', function(event) {
-    menu.removeClass('header__menu-active');
+$('.hamburger__menu--back-to-top').on('click', function(event) {
+    menu.removeClass('hamburger__menu-active');
 });
